65b48c55b3a469f826434ce30163165102d9e7aa
Jedrzej Serwa <jedrzej.serwa@travelportdigirtal.com>
2019-03-30 18:06:17 +0100

    Updated IntegrationTest base class

--- src/test/groovy/dev/serwa/in2it/IntegrationTest.groovy
+++ src/test/groovy/dev/serwa/in2it/IntegrationTest.groovy
@@ -1,8 +1,25 @@
 package dev.serwa.in2it
 
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.client.TestRestTemplate
+import redis.embedded.RedisServer
 import spock.lang.Specification
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 abstract class IntegrationTest extends Specification {
+
+  @Autowired
+  protected TestRestTemplate testRestTemplate
+
+  private static RedisServer redisServer
+
+  void setupSpec() {
+    redisServer = new RedisServer(6063)
+    redisServer.start()
+  }
+
+  void cleanupSpec() {
+    redisServer.stop()
+  }
 }
